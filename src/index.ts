5468f4af99b9b2cc22e697b9d0e0250794362bdd
heyhey123-git <156066831+heyhey123-git@users.noreply.github.com>
2024-05-27 03:49:43 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 // LiteLoader-AIDS automatic generated
-/// <reference path="../../dist/types/src/index.d.ts"/>
-// LiteLoader-AIDS automatic generated
-/// <reference path="i:\Levilamina-Aids/dts/HelperLib-master/src/index.d.ts"/> 
-
+/// <reference path="../dist/types/src/index.d.ts"/>
 mc.listen('onServerStarted', () => {
     require('./lib/apiExporter');
 });
