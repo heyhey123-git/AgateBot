b87942b27a164bab991d926c43347e27b633e693
heyhey123-git <156066831+heyhey123-git@users.noreply.github.com>
2024-05-27 11:46:12 +0800

    Delete dist directory

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// LiteLoader-AIDS automatic generated
-/// <reference path="d:\Levilamina/dts/HelperLib-master/src/index.d.ts"/>
-mc.listen('onServerStarted', () => {
-    require('./lib/configCreator');
-});
-//# sourceMappingURL=index.js.map
